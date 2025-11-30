7a77dcb4749f146ab864237a670bae56962a667e
CosmicRoboCat <85073940+DmitryR1986@users.noreply.github.com>
2025-12-01 01:38:40 +0300

    Create winSysInfo.cpp

---
+++ Chapter 1/Exercise 1/winSysInfo.cpp
@@ -0,0 +1,152 @@
+/*	
+	Exercise 1  from "Windows 10 System Programming" (by Pavel Yosifovich), Chapter 1:
+
+	"Write a console application that prints more information about the system than the
+	HelloWin application shown earlier, by calling the following APIs: 
+	GetNativeSystemInfo, GetComputerName, GetWindowsDirectory, QueryPerformanceCounter, 
+	GetProductInfo, GetComputerObjectName. Handle errors if they occur."
+	
+	NOTE 1: Make sure to create manifest.xml file for getting modern OS versions above 6th.
+	Project -> Properties -> Manifest Tool -> Input And Output -> Additional Manifest Files: manifest.xml
+
+	NOTE 2: For GetComputerObjectNameW function 1) include <security.h> header file and 
+	2) add Secur32.lib to Additional Dependencies:
+	Project -> Properties -> Linker --> Input --> Additional Dependencies: Secur32.lib
+	3) add #define SECURITY_WIN32 in source code
+*/
+
+#define SECURITY_WIN32
+#define BUILD_WINDOWS
+#include <Windows.h>
+#include <security.h>
+#include <stdio.h>
+
+void ErrorCodeAndMeaning(DWORD *lastError) {
+	PWSTR errorMeaning;
+	DWORD chars = ::FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		nullptr, *lastError, 0, (PWSTR)&errorMeaning, 0, nullptr);
+	if (chars > 0) {
+		printf("Error %lu: %ws\n", *lastError, errorMeaning);
+		::LocalFree(errorMeaning);
+	}
+}
+
+PCWSTR GetWindowsVersion(DWORD* productType)
+{
+	switch (*productType) {
+	case PRODUCT_HOME_BASIC:
+		return L"Windows Home";
+		break;
+	case PRODUCT_PROFESSIONAL:
+		return L"Windows Professional";
+		break;
+	case PRODUCT_ENTERPRISE:
+		return L"Windows Enterprise";
+		break;
+	case PRODUCT_EDUCATION:
+		return L"Windows Education";
+		break;
+	default:
+		return L"Unknown product type";
+		break;
+	}
+}
+
+
+int main(int argc, const char* argv[]) {
+	
+	DWORD lastError;
+
+	SYSTEM_INFO si;
+	::GetNativeSystemInfo(&si);
+
+	printf("Processor Type: %lu\n", si.dwProcessorType);
+	printf("Number of Logical Processors: %d\n", si.dwNumberOfProcessors);
+	printf("Page size: %d Bytes\n", si.dwPageSize);
+	//printf("Processor Mask: 0x%p\n", (PVOID)si.dwActiveProcessorMask);
+	#ifdef _WIN64
+		printf("Processor Mask x64: 0x%016llX\n", si.dwActiveProcessorMask);
+	#else
+		printf("Processor Mask x32: 0x%08X\n", si.dwActiveProcessorMask);
+	#endif
+	printf("Minimum process address: 0x%p\n", si.lpMinimumApplicationAddress);
+	printf("Maximum process address: 0x%p\n", si.lpMaximumApplicationAddress);
+
+
+	WCHAR infoBuf[MAX_PATH];
+	DWORD size = MAX_PATH;
+	if (!::GetComputerNameW(infoBuf, &size)) {
+		lastError = ::GetLastError();
+		ErrorCodeAndMeaning(&lastError);
+	}
+	else {
+		printf("Computer Name: %ws\n", infoBuf);
+	}
+
+	WCHAR netBiosName[MAX_PATH];
+	size = sizeof(netBiosName) / sizeof(WCHAR);
+	if (::GetComputerNameExW(ComputerNameNetBIOS, netBiosName, &size))
+		wprintf(L"NetBIOS name: %ws\n", netBiosName);
+	else {
+		lastError = ::GetLastError();
+		ErrorCodeAndMeaning(&lastError);
+	}
+
+
+	if (!::GetWindowsDirectoryW(infoBuf, MAX_PATH)) {
+		lastError = ::GetLastError();
+		ErrorCodeAndMeaning(&lastError);
+	}
+	else {
+		printf("Windows Directory: %ws\n", infoBuf);
+	}
+
+	LARGE_INTEGER performanceCount;
+	if (!::QueryPerformanceCounter(&performanceCount)) {
+		lastError = ::GetLastError();
+		ErrorCodeAndMeaning(&lastError);
+	}
+	else {
+		printf("Performance Counter value: %llu\n", performanceCount.QuadPart);
+	}
+	
+	if (!::GetSystemDirectoryW(infoBuf, MAX_PATH)) {
+		lastError = ::GetLastError();
+		ErrorCodeAndMeaning(&lastError);
+	}
+	else {
+		printf("System directory: %ws\n", infoBuf);
+	}
+
+	OSVERSIONINFOEXW vi = { sizeof(vi) };
+	::GetVersionExW((LPOSVERSIONINFOW)&vi);
+	DWORD productType;
+	if (::GetProductInfo(vi.dwMajorVersion, vi.dwMinorVersion, vi.wServicePackMajor, vi.wServicePackMinor, &productType)) {
+		wprintf(L"%ws, ", GetWindowsVersion(&productType));
+		printf("version: %lu.%lu.%lu\n", vi.dwMajorVersion, vi.dwMinorVersion, vi.dwBuildNumber);
+	}
+
+	//==========  GetComputerObjectName ==========
+	ULONG size2 = 0;
+	PWSTR pComputerName;
+	//1. using nullptr instead of buffer (2nd parameter) gives us right size of NameDisplay according to Microsoft specification
+	::GetComputerObjectNameW(NameDisplay, nullptr, &size2);
+	pComputerName = new WCHAR[size2]; //allocate memory of right size for NameDisplay
+	if (!pComputerName) {
+		wprintf(L"Memory allocation failed\n");
+		exit(0);
+	}
+	//2. finally save NameDisplay in allocated buffer
+	if (::GetComputerObjectNameW(NameDisplay, pComputerName, &size2)) {
+		wprintf(L"NameDisplay: %ws\n", pComputerName);
+	}
+	else {
+		wprintf(L"NameDisplay -- ");
+		lastError = ::GetLastError();
+		ErrorCodeAndMeaning(&lastError);
+	}
+	delete[] pComputerName;
+	
+
+	return 0;
+}
